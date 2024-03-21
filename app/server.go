59092c6217abd7421821d91e4c7458a5d9ff73e5
Surya K N <95220715+Surya-KN@users.noreply.github.com>
2024-03-21 20:57:47 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -25,6 +27,22 @@ func main() {
 	os.Exit(1)
 
 	}
-  conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+  buf := make([]byte,5000)
+  _ , err = conn.Read(buf)
+
+  if err != nil {
+	fmt.Println("Error reading:", err.Error())
+	os.Exit(1)
+  }
+  fmt.Println(string(buf))
+
+  path := strings.Split(string(buf), "\r\n")
+  path = strings.Split(path[0], " ")
+  if path[1] == "/" {
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+  } else {
+	conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+  }
+  
   conn.Close()
 }
